2e5a5ec947d5a596240ce720732ec412f8104a41
Elaina1314 <100994314+Shiiyuko@users.noreply.github.com>
2024-07-06 21:49:53 +0800

    fix error: unmappable character

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,11 @@ repositories {
     mavenCentral()
 }
 
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
